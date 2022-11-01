20ca254d2a14f97825b43f3ccf551f6063f8f532
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-01 11:34:36 +0000

    word time in red

--- clock/Clock.java
+++ clock/Clock.java
@@ -5,6 +5,6 @@ class Clock {
   public static void main(String[] args) {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
     LocalDateTime now = LocalDateTime.now();
-    System.out.printf("The current time is %s \n", dtf.format(now));
+    System.out.printf("The current \033[0;31mtime \033[0mis %s \n", dtf.format(now));
   }
 }
