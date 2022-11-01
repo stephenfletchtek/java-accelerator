d6b3ac2458319fdad54602e637ca94ae32b04a78
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-01 12:08:23 +0000

    restructure folders

--- first_program/Clock.java
+++ first_program/Clock.java
@@ -2,9 +2,11 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 class Clock {
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_RESET = "\u001B[0m";
   public static void main(String[] args) {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
     LocalDateTime now = LocalDateTime.now();
-    System.out.printf("The current \033[0;31mtime \033[0mis %s \n", dtf.format(now));
+    System.out.printf("The current " + ANSI_RED + "time " + ANSI_RESET + "is %s \n", dtf.format(now));
   }
 }
