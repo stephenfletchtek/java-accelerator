1295f34d17f24be505bc161a6e42233f4c104be6
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-01 15:46:41 +0000

    first test of a calculator

--- calculator/app/src/main/java/calculator/Calculator.java
+++ calculator/app/src/main/java/calculator/Calculator.java
@@ -1,9 +1,7 @@
 package calculator; 
 
-
 class Calculator {
-  public static void main(String[] args) {
-    Integer result = 2 + 2;
-    System.out.printf("The result is %d \n", result);
+  Integer add(Integer int1, Integer int2) {
+    return int1 + int2;
   }
 }
