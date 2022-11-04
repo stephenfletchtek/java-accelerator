b83455f5b4e6442f05b7c2418dd90784ef0a662c
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-04 13:57:35 +0000

    building tests for todo list

---
+++ 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
@@ -0,0 +1,13 @@
+package todo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TodoTest {
+  // Initialised with empty list of tasks
+
+  // addTask takes a String and adds it to the list
+
+  // getTasks returns list of tasks
+}
