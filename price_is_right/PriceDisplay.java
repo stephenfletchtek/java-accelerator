0d10c8fe0bf46f0b47de3bf77c9bea8168f241d8
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-04 09:53:46 +0000

    add \n to printf string and rename file to PriceDispaly.java

--- price_is_right/PriceDisplay.java
+++ price_is_right/PriceDisplay.java
@@ -2,6 +2,7 @@ class PriceDisplay {
   public static void main(String[] args){
     String item = "Teapot";
     Double price = 10.99;
-    System.out.printf("The price for the item %s is %.2f", item, price); 
+
+    System.out.printf("The price for the item %s is %.2f \n", item, price); 
   }
 }
