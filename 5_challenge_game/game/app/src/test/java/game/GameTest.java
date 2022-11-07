36d0d6baaf5438ed69ed59b3368c570cb943a6dc
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-07 20:39:27 +0000

    trying to test test to run

--- 5_challenge_game/game/app/src/test/java/game/GameTest.java
+++ 5_challenge_game/game/app/src/test/java/game/GameTest.java
@@ -5,8 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public abstract class GameTest {
-  @Test
-  public void testGetWordToGuess() {
+  @Test public void testGetWordToGuess() {
     Game game = new Game();
     assertEquals(game.getWordToGuess(), "B____");
   }
