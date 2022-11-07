738700d287e5c2d0cc652099bb8e35e51906f61e
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-07 14:59:45 +0000

    assertTrue for empty list

--- 4_testing_with_junit/todo/app/src/main/java/todo/Todo.java
+++ 4_testing_with_junit/todo/app/src/main/java/todo/Todo.java
@@ -3,7 +3,7 @@ package todo;
 import java.util.*;
 
 public class Todo {
-  private static List<String> todoList = new ArrayList<String>();
+  List<String> todoList = new ArrayList<String>();
 
   public List<String> getTasks() {
     return todoList;
--- 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
+++ 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
@@ -1,6 +1,6 @@
 package todo;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import java.util.List;
@@ -10,6 +10,7 @@ public class TodoTest {
   @Test public void instantiate() { 
     Todo todo = new Todo();
     assertEquals("Empty todo list", List.of(), todo.getTasks());
+    assertTrue("Empty todo list", todo.todoList.isEmpty());
   }
 
   // addTask takes a String and adds it to the list
@@ -22,8 +23,9 @@ public class TodoTest {
   // getTasks returns list of tasks
   @Test public void threeTasks() {
     Todo todo = new Todo();
+    todo.addTask(("first task"));
     todo.addTask("second task");
     todo.addTask("third task");
-    assertEquals("list of three tasks", List.of("First task", "second task", "third task"), todo.getTasks());
+    assertEquals("list of three tasks", List.of("first task", "second task", "third task"), todo.getTasks());
   }
 }
