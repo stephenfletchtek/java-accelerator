f714bab3b176d9429dc47c43ebe17b5d016b0fac
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-07 20:33:25 +0000

    first test getWordToGuess()

---
+++ 5_challenge_game/game/app/src/test/java/game/GameTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public abstract class GameTest {
+  @Test
+  public void testGetWordToGuess() {
+    Game game = new Game();
+    assertEquals(game.getWordToGuess(), "B____");
+  }
+}
