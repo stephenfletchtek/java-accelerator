e28c316946f04bfa454a2b450934037e7d62cd40
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-07 09:25:45 +0000

    written failing test to instantiate todoList class

--- 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
+++ 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TodoTest {
-  // Initialised with empty list of tasks
+  @Test public void instantiate() {
+    // Initialised with empty list of tasks
+    TodoList todoList = new TodoList();
+    asserEquals("Empty todo list", String.valueOf(""), todoList.getTasks());
+  }
 
   // addTask takes a String and adds it to the list
 
