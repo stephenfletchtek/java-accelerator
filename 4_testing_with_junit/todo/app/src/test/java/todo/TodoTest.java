eaa8b5d3aab866b00c35b296aa23ff3a119e385a
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-07 09:30:06 +0000

    class Todo created but has no methods

--- 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
+++ 4_testing_with_junit/todo/app/src/test/java/todo/TodoTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class TodoTest {
   @Test public void instantiate() {
     // Initialised with empty list of tasks
-    TodoList todoList = new TodoList();
-    asserEquals("Empty todo list", String.valueOf(""), todoList.getTasks());
+    Todo todo = new Todo();
+    asserEquals("Empty todo list", String.valueOf(""), todo.getTasks());
   }
 
   // addTask takes a String and adds it to the list
