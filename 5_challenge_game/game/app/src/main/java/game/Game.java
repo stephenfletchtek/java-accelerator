e57892f2cb7aa33b641249df2177028fc1235daf
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-08 09:15:46 +0000

    stringbuider working and makers version in the comments

--- 5_challenge_game/game/app/src/main/java/game/Game.java
+++ 5_challenge_game/game/app/src/main/java/game/Game.java
@@ -1,16 +1,36 @@
 package game;
 
 public class Game { 
-  String wordToGuess;
   StringBuilder stringBuilder;
 
   Game(String word) {
-    super();
-    stringBuilder = new StringBuilder(word);
+    this.stringBuilder = new StringBuilder(word);
   }
 
-  public StringBuilder getWordToGuess() {
-    stringBuilder.replace(1,6,"_____");
-    return stringBuilder;
+  public String getWordToGuess() {
+    int max = this.stringBuilder.length();
+    for (int i = 1; i < max; i++) {
+      this.stringBuilder.replace(i, i + 1, "_");
+    }
+    return this.stringBuilder.toString();
   }
+
+
+  // Makers version
+  // String word;
+  // Game(String word) {
+  //   this.word = word;
+  // }
+  // public String getWordToGuess() {
+  //   StringBuilder builder = new StringBuilder();
+  //   for (int i = 0; i < this.word.length(); i++) {
+  //       Character currentLetter = word.charAt(0);
+  //       if (i == 0) {
+  //           builder.append(currentLetter);
+  //       } else {
+  //           builder.append("_");
+  //       }
+  //   }
+  //   return builder.toString();
+  // }
 }
